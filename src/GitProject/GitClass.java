12ade2dd8111cf66c2b843e0e1b83f0677385b54
epavaus <epavaus@PF0CAX8G.ericsson.se>
2017-12-19 11:21:10 +0530

    new line added

--- src/GitProject/GitClass.java
+++ src/GitProject/GitClass.java
@@ -3,6 +3,7 @@ package GitProject;
 public class GitClass {
 	static void gitMethod() {
 		System.out.println("This is GIT Method 1");
+		System.out.println("This is GIT Method 2");
 	}
 
 	public static void main(String[] args) {
