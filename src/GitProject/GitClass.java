cff5e015fc0c8ed10c7aa6d76cbb2a87302e2ac3
epavaus <epavaus@PF0CAX8G.ericsson.se>
2017-12-19 11:20:28 +0530

    My new GIT project

---
+++ src/GitProject/GitClass.java
@@ -0,0 +1,13 @@
+package GitProject;
+
+public class GitClass {
+	static void gitMethod() {
+		System.out.println("This is GIT Method 1");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		gitMethod();
+	}
+
+}
